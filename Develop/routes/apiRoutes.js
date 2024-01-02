6265cf072095a81ee9fcff5b2f1c07ee3a61096f
unknown <dannysaravia100@gmail.com>
2024-01-01 21:08:46 -0500

    added GET function to api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,3 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express'); 
 const {v4: uuidv4} = require('uuid'); 
+
+const router = express.Router();
+const dbPath = path.join(__dirname,'../db.json');
+
+//GET routes to fetch notes 
+
+router.get('/notes', (req, res) => {
+    try {
+        const notes = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+        res.json(notes);
+    } catch (error) {
+       console.error(error);
+       res.status(500).send('Server Error');
+    }
+});
+
