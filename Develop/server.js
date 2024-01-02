78052b215b926a6bdb7ebae7b4671cd82b102c3b
unknown <dannysaravia100@gmail.com>
2024-01-01 20:05:17 -0500

    added express app to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,3 +2,10 @@ const express = require('express')
 const htmlRoutes = require('./routes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes')
 const path = require('path')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(expreess.json());
+app.use(express.urlencoded({ extended: true}));
+
