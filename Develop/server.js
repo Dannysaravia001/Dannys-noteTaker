390ca9f1650d2bfb6c69a9c3f9145e7bdcaa091c
unknown <dannysaravia100@gmail.com>
2024-01-01 19:56:48 -0500

    finished adding const routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
 const express = require('express')
-const htmlRoutes = require('')
-const apiRoutes = require('')
-const path = require('')
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
+const path = require('path')
